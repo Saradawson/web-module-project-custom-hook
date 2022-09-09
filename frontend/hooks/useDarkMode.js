f784d8901ac46c696a20e40ef2c53415782b3ce2
unknown <saradawsone@gmail.com>
2022-09-09 13:21:57 -0600

    useDarkMode hook built

---
+++ frontend/hooks/useDarkMode.js
@@ -0,0 +1,6 @@
+import { useState } from 'react';
+
+export const useDarkMode = (initialValue) => {
+    const [darkMode, setDarkMode] = useState(initialValue);
+    return [darkMode, setDarkMode];
+}
